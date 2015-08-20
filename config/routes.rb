18e5c3fca0729054c36b4f029da84ad74728e509
t <onlyyouandty@gmail.com>
2015-08-20 23:19:19 +0100

    let's host something

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admins
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :products
-  root 'products#index'
+  root 'high_voltage/pages#show', :id => 'home'
+  
   resources :messages, only: [:new, :create]
 end
