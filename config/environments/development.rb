03dac58b0ac7f7292e044841a19a152f4e433f4e
t <onlyyouandty@gmail.com>
2015-08-21 09:02:01 +0100

    mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,21 +44,16 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # ActionMailer Config
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  config.action_mailer.delivery_method = :smtp
-  # change to true to allow email to be sent during development
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
+  config.assets.debug = true
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
 
-  # Gmail  
-    config.action_mailer.smtp_settings = {
-    :port                 => 587,
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
-    # :domain               => 'gmail.com',
-    :user_name            => 'onlyyouandty@gmail.com',
-    :password             => 'theHeadright153',
-    :enable_starttls_auto => true,
-    :authentication       => 'plain'
-  }
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'ariesdevelop@gmail.com',
+    :password             => 'Aries@develop',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,19 +81,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
     # Required for Devise. Remember to change localhost:3000 to actual application host
-  config.mandrill_mailer.default_url_options = { :host => 'intaccess.herokuapp.com' }
-  # ActionMailer Config
-  # Setup for production - deliveries, no errors raised
+  config.assets.debug = true
+  config.action_mailer.default_url_options = {:host => 'https://intaccess.herokuapp.com/'}
+
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.default :charset => "utf-8"
-  
   config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
     :port                 => 587,
-    :address              => "smtp.mandrillapp.com",
-    :domain               => 'mandrillapp.com',
-    :user_name            => ENV["MANDRILL_USERNAME"],
-    :password             => ENV["MANDRILL_APIKEY"],
-    :authentication       => 'plain'
-  }
+    :domain               => 'gmail.com',
+    :user_name            => 'ariesdevelop@gmail.com',
+    :password             => 'Aries@develop',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
 end
